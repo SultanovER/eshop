fbdf2c0421f09f29fbfb7e58e38018eadf54fe6b
SultanovER <sae160305@gmail.com>
2024-07-30 01:45:06 +0600

    4work

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', views.main_page_view, name='product_list'),
     path('', views.main_page_view),
     path('products/<int:id>/', views.product_detail_view, name='product_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- eshop/main/models.py
+++ eshop/main/models.py
@@ -14,13 +14,14 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
+    image = models.ImageField(null=True, upload_to='images')
     sizes = models.ManyToManyField(Size, verbose_name='объём', blank=True)
     title = models.CharField(max_length=255, verbose_name='Название')
     description = models.TextField(verbose_name='Модель')
     price = models.FloatField(verbose_name='Цена')
     is_active = models.BooleanField(default=True, verbose_name='Активен')
     created = models.DateTimeField(null=True, verbose_name='Дата создания')
-    updated = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата изменения') 
     
     def __str__(self):
         return self.title
